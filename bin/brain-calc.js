cb1cd7d91f4ebe9540d6bdc57b07502fd85414a8
Soshinoya <nike95125@gmail.com>
2024-02-08 12:02:51 +0300

    7 step

---
+++ bin/brain-calc.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import startCalc from '../src/ggs/calc.js';
+
+startCalc();
---
+++ src/ggs/calc.js
@@ -0,0 +1,31 @@
+import playGame from '../inde.js';
+import getRandomNumber from '../randomizer.js';
+
+const task = 'What is the result of the expression? ';
+
+const calculate = (num1, num2, operation) => {
+  switch (operation) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      throw new Error(`Unknown order state: '${operation}'!`);
+  }
+};
+const playCalc = () => {
+  const first = getRandomNumber();
+  const second = getRandomNumber();
+  const operators = ['+', '-', '*'];
+  const operator = operators[getRandomNumber(0, operators.length - 1)];
+  const question = `${first} ${operator} ${second}`;
+  const correctAnswer = calculate(first, second, operator).toString();
+  return [question, correctAnswer];
+};
+const startCalc = () => {
+  playGame(playCalc, task);
+};
+
+export default startCalc;
