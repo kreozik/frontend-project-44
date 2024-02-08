42e73820a651b465941b4a6839cc5f2812ad4794
Soshinoya <nike95125@gmail.com>
2024-02-08 12:15:11 +0300

    8sp

---
+++ bin/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import startGCD from '../src/ggs/GCD.js';
+
+startGCD();
---
+++ src/ggs/GCD.js
@@ -0,0 +1,20 @@
+import playGame from '../inde.js';
+import getRandomNumber from '../randomizer.js';
+
+const task = 'Find the greatest common divisor of given numbers.';
+
+const getGcd = (x, y) => ((y !== 0) ? getGcd(y, x % y) : x);
+
+const playGCD = () => {
+  const first = getRandomNumber();
+  const second = getRandomNumber();
+  const question = `${first} ${second}`;
+  const correctAnswer = String(getGcd(first, second));
+  return [question, correctAnswer];
+};
+
+const startGCD = () => {
+  playGame(playGCD, task);
+};
+
+export default startGCD;
